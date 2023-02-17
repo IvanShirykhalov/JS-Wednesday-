ea303870e92cfc74a536ec0c824e78bd95c67861
Ivan Shirykhalov <ivanshirykhalov@gmail.com>
2023-02-17 23:32:14 +0300

    promise

--- src/lessons/lesson3/promise.js
+++ src/lessons/lesson3/promise.js
@@ -1,98 +1,25 @@
-let promise = {}
+let parsedGoogleData = null;
+let dataFromIncubator = null
 
-promise.then(() => {
-}) // -> 'resolved'
-promise.catch(() => {
-}) // -> 'rejected'
-promise.finally()
-//promise._status = 'pending' // 'resolved', 'rejected'
-
-//промис это объет, который имеет три состояния 'pending', 'resolved' и 'rejected'
-// и три метода then, catch и finally
-//с помощью then и catch мы подписываемся на событие
-
-
-//ассинхронный код (fetch, setInterval, setTimeout)
-
-
-const promise = fetch('https://google.com', (googleData))
-
-promise.then((data) => {
-
-})
-
-promise1 = wait(10000)
-
-// const thenPromise2 = promise1.then(() => {
-//     console.log('subscriber for promise1 called')
-// })
-// const thenPromise3 = thenPromise2.then(() => {
-//     console.log('subscriber for promise2 called')
-//
-// })
-
-// promise1
-//     .then(() => {
-//         console.log('subscriber for promise1 called')
-//     })
-//     .then(() => {
-//         console.log('subscriber for promise2 called')
-//
-//     })
-
-
-// promise1
-//     .then(() => {
-//         console.log('subscriber for promise1 called')
-//         const someNewPromise = wait(1000)
-//         return someNewPromise
-//     })
-//     .then(() => {
-//         console.log('subscriber for promise2 called')
-//
-//     })
-//     .then(() => {
-//         console.log('subscriber for promise3 called')
-//         return {value: 100}
-//
-//     }).then((objWithValue) => {})
-
-// fetch('https://google.com')
-//     .then(() => {
-//         console.log('subscriber for promise1 called')
-//         const someNewPromise = wait(1000)
-//         return someNewPromise
-//     })
-//     .then(() => {
-//         console.log('subscriber for promise2 called')
-//
-//     })
-//     .then(() => {
-//         console.log('subscriber for promise3 called')
-//         return {value: 100}
-//
-//     }).then((objWithValue) => {})
-
-
-const run = async () => {
-
-    const dataFromGoogle = await fetch('https://google.com')
-    const parseGoogleData = parseGoogleResult(dataFromGoogle)
-    const dataFromIncubator = await fetch(parseGoogleData[0])
-    const dataFromHexlet = await fetch(parseGoogleData[1])
-    const finalData = await processData(dataFromIncubator, dataFromHexlet)
-
-    console.log('SUCCESS')
-}
-
-run()
-
-
-function wait(ms) {
-    return new Promise((resolveFunction) => {
-        setTimeout(() => {
-            console.log('before resolving')
-            resolveFunction()
-        }, ms)
+fetch('https://google.com')
+    .then((data) => {
+        parsedGoogleData = parseGoogleResult(data)
+        return fetch(parsedGoogleData[0])
     })
+    .then((data) => {
+        dataFromIncubator = data
+        return fetch(parsedGoogleData[1])
+    })
+    .then((dataFromHexlet) => {
+        return processData(dataFromIncubator, dataFromHexlet)
+    })
+    .then(function processDataSuccessHandler(finalData) {
+        console.log("SUCCESS")
+    })
+
+
+
+// 3
+function processData(data1, data2, callback) {
+    // AI, analysis
 }
